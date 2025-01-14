47d26de80439b7af7c0bd32cd129de072d3a2874
clomose <smartbroutkarsh@gmail.com>
2025-01-14 11:08:26 +0530

    add solution for finding the maximum number of content children

---
+++ Atlassian/LeetCode455.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int a = g.length-1;
+        int b = s.length-1;
+        int ans = 0;
+        while(a>=0 && b>=0){
+            if(g[a]<=s[b]){
+                a--;
+                b--;
+                ans++;
+            }else{
+                a--;
+            }
+        }
+        return ans;
+    }
+}
